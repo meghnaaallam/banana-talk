741c0f71b5187daff27fde0b5d5eb202a5ec1e13
Meghna Reddy <53735437+meghnareddy1999@users.noreply.github.com>
2022-01-15 18:25:40 +0530

    upto-eventlisteners
    addeventlistener
    queryselector
    reading the value
    innerText

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-var userName= prompt("please enter your name:")
-alert("hello "+ userName +", you're writing this in JS");
+var btnTranslate=document.querySelector("#btn-translate")
+var userInput=document.querySelector("#user-text")
+var outputTxt= document.querySelector("#output-txt")
+
+outputTxt.innerText= "hello! she is awesome!" //writing inside the element
+
+
+btnTranslate.addEventListener("click", clickHandler) //listening to the event
+
+
+
+
+
+function clickHandler() {
+    console.log("hello i am clicked!")
+    console.log(userInput.value)
+    console.log(outputTxt.value) //reading 
+};
