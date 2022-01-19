b802173c164c9405dc1cc0d1aa1a96dfe0b1c547
Meghna Reddy <53735437+meghnareddy1999@users.noreply.github.com>
2022-01-19 19:03:17 +0530

    js: done

--- app.js
+++ app.js
@@ -2,17 +2,31 @@ var btnTranslate=document.querySelector("#btn-translate")
 var userInput=document.querySelector("#user-text")
 var outputTxt= document.querySelector("#output-txt")
 
-outputTxt.innerText= "hello! she is awesome!" //writing inside the element
+//mock server
+//url= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 
+var minionURL= "https://api.funtranslations.com/translate/minion.json"
+
+function constructURL(text) {
+    return minionURL+ "?"+ "text=" + text 
+}
+
+function errorHandler(error) {
+    console.log("something is wrong, please try again", error)
+}
 btnTranslate.addEventListener("click", clickHandler) //listening to the event
 
 
-
-
-
 function clickHandler() {
-    console.log("hello i am clicked!")
-    console.log(userInput.value)
-    console.log(outputTxt.value) //reading 
+    var inputTxt = userInput.value; //reading input
+
+    //processing
+    fetch(constructURL(inputTxt))
+    .then((response) => response.json()) 
+    .then(translate => {
+        var output = translate.contents.translated
+        outputTxt.innerText=output
+    })
+    .catch(errorHandler)
 };
